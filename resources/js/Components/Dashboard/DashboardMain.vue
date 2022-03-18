35e43bf3355338cf9889e9d850acf6d333379334
HkFromMY <bpvper@gmail.com>
2022-03-18 18:26:29 +0800

    dashboard tweaking

--- resources/js/Components/Dashboard/DashboardMain.vue
+++ resources/js/Components/Dashboard/DashboardMain.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <!-- Recruiter dashboard -->
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" v-if="$page.props.user.current_team_id==2">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="inline-block" style="max-width: 300px">
                     <vue3-chart-js v-bind="{ ...pieChart }" />
@@ -27,11 +28,11 @@ import Vue3ChartJs from "@j-t-mcc/vue3-chartjs";
 const pieChart = {
     type: "pie",
     data: {
-        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
+        labels: ["Internship", "Part Time", "Full Time", "Contract"],
         datasets:[
         {
             backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-            data: [40, 20, 80, 10],
+            data: [10, 2, 8, 5],
         },
         ],
     },
@@ -41,7 +42,7 @@ const doughnutChart = {
     id: 'doughnut',
     type: 'doughnut',
     data: {
-    labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+    labels: ["Internship", "Part Time", "Full Time", "Contract"],
     datasets: [
         {
         backgroundColor: [
@@ -50,7 +51,7 @@ const doughnutChart = {
             '#00D8FF',
             '#DD1B16'
         ],
-        data: [40, 20, 80, 10]
+        data: [10, 2, 8, 5]
         }
     ]
     }
@@ -59,154 +60,171 @@ const doughnutChart = {
 const barChart = {
     type: "bar",
     options: {
-    min: 0,
-    max: 100,
-    responsive: true,
-    plugins: {
-        legend: {
-        position: "top",
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                font: {
+                    size: 24,
+                    weight: 'bold'
+                },
+                text: "Distribution of Job Post among Job Post's Age"
+            }
         },
-    },
-    scales: {
-        y: {
-        min: -100,
-        max: 100,
-        ticks: {
-            callback: function (value) {
-            return `${value}%`;
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    callback: function (value) {
+                        return `${value}`; // y-ticks
+                    },
+                },
             },
         },
-        },
-    },
     },
     data: {
-    labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
-    datasets: [
-        {
-        label: "My First Dataset",
-        backgroundColor: ["#1abc9c", "#f1c40f", "#2980b9", "#34495e"],
-        data: [40, 20, 50, 10],
-        },
-        {
-        label: "My Second Dataset",
-        backgroundColor: ["#2ecc71", "#e67e22", "#9b59b6", "#bdc3c7"],
-        data: [-40, -20, -10, -10],
-        },
-    ],
+        labels: ["Less than 3 Months", "3 - 6 Months", "6 - 12 Months", "More than a year"],
+        datasets: [
+            {   
+                backgroundColor: ["#1abc9c", "#f1c40f", "#2980b9", "#34495e"],
+                data: [120, 87, 32, 12],
+            },
+        ],
     },
 };
 
 const radarChart = {
     type: "radar",
     options: {
-    min: 0,
-    max: 100,
-    responsive: true,
-    plugins: {
-        legend: {
-        position: "top",
-        },
-    },
-    scales: {
-        y: {
-        min: -100,
+        min: 0,
         max: 100,
-        ticks: {
-            callback: function (value) {
-            return `${value}%`;
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                font: {
+                    size: 24,
+                    weight: 'bold'
+                },
+                text: 'Average Qualified Candidates per Job Opening'
+            },
+            legend: {
+                display: false,
             },
         },
+        scales: {
+            y: {
+                min: 0,
+                max: 100,
+                ticks: {
+                    callback: function (value) {
+                        return `${value}`;
+                    },
+                },
+            },
         },
-    },
     },
     data: {
-    labels: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-    ],
-    datasets: [
-        {
-        label: "Skip first dataset",
-        data: [
-            null,
-            78.59825103,
-            8.49108368,
-            40.75017147,
-            95.12860082,
-            43.23131001,
-            19.62620027,
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
         ],
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-        },
-        {
-        label: "Skip mid dataset",
-        data: [
-            99.62191358,
-            43.71227709,
-            8.68741427,
-            null,
-            43.26731824,
-            41.26714678,
-            5.83076132,
+        datasets: [
+            {
+                label: "Skip first dataset",
+                data: [
+                    24.1231,
+                    78.59825103,
+                    8.49108368,
+                    40.75017147,
+                    95.12860082,
+                    43.23131001,
+                    19.62620027,
+                ],
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+            // {
+            //     label: "Skip mid dataset",
+            //     data: [
+            //         99.62191358,
+            //         43.71227709,
+            //         8.68741427,
+            //         null,
+            //         43.26731824,
+            //         41.26714678,
+            //         5.83076132,
+            //     ],
+            //     borderColor: "rgb(54, 162, 235)",
+            //     backgroundColor: "rgba(54, 162, 235, 0.5)",
+            // },
+            // {
+            //     label: "Skip last dataset",
+            //     data: [
+            //         75.2297668,
+            //         50.69873114,
+            //         74.21296296,
+            //         72.9329561,
+            //         70.31550069,
+            //         63.89146091,
+            //         null,
+            //     ],
+            //     borderColor: "rgb(75, 192, 192)",
+            //     backgroundColor: "rgba(75, 192, 192, 0.5)",
+            // },
         ],
-        borderColor: "rgb(54, 162, 235)",
-        backgroundColor: "rgba(54, 162, 235, 0.5)",
-        },
-        {
-        label: "Skip last dataset",
-        data: [
-            75.2297668,
-            50.69873114,
-            74.21296296,
-            72.9329561,
-            70.31550069,
-            63.89146091,
-            null,
-        ],
-        borderColor: "rgb(75, 192, 192)",
-        backgroundColor: "rgba(75, 192, 192, 0.5)",
-        },
-    ],
     },
 };
 
 const lineChart = {
     type: "line",
     data: {
-    labels: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-    ],
-    datasets: [
-        {
-        label: "Caffine Consumption",
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: "#41B883",
-        backgroundColor: "black",
-        },
-        {
-        label: "Productivity",
-        data: [70, 25, 110, 90, 5, 60, 30],
-        fill: false,
-        borderColor: "#00D8FF",
-        tension: 0.5,
-        backgroundColor: "blue",
-        },
-    ],
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+        ],
+        datasets: [
+            {
+                label: "Views per Job Post",
+                data: [20, 23, 22, 18, 34, 40, 36],
+                fill: false,
+                borderColor: "#41B883",
+                backgroundColor: "black",
+            },
+            {
+                label: "Advertisement Post",
+                data: [3, 4, 1, 5, 10, 12, 10],
+                fill: false,
+                borderColor: "#00D8FF",
+                tension: 0.5,
+                backgroundColor: "blue",
+            },
+        ],
     },
     options: {
+        plugins: {
+            title: {
+                display: true,
+                font: {
+                    size: 24,
+                    weight: 'bold'
+                },
+                text: 'Views per Job Post vs Advertisement Posted'
+            }
+        }
     },
 };
 
